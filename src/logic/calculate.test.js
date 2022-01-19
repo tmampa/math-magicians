9355d392bc90ed622ba6022d5a97a38c8b55e325
abdessalem1998 <89970442+abdessalem1998@users.noreply.github.com>
2022-01-19 12:29:35 +0100

    unit test for calculate files all operations

---
+++ src/logic/calculate.test.js
@@ -0,0 +1,105 @@
+import calculate from './calculate';
+
+
+describe('unit tests for calculate.js', () => {
+  test('= is clicked the result should be addition', () => {
+    //Arrange
+    const obj = {
+      total: 100,
+      next: 20,
+      operation: '+',
+    };
+    const buttonName = '=';
+
+    //Act
+    const result = calculate(obj, buttonName);
+
+    //Assert
+    expect(result).toStrictEqual({
+      total: '120',
+      next: null,
+      operation: null,
+    });
+  });
+
+  test('= is clicked the result should be subtraction', () => {
+    //Arrange
+    const obj = {
+      total: 100,
+      next: 20,
+      operation: '-',
+    };
+    const buttonName = '=';
+
+    //Act
+    const result = calculate(obj, buttonName);
+
+    //Assert
+    expect(result).toStrictEqual({
+      total: '80',
+      next: null,
+      operation: null,
+    });
+  });
+
+  test('= is clicked the result should be multiplication', () => {
+    //Arrange
+    const obj = {
+      total: 100,
+      next: 20,
+      operation: 'x',
+    };
+    const buttonName = '=';
+
+    //Act
+    const result = calculate(obj, buttonName);
+
+    //Assert
+    expect(result).toStrictEqual({
+      total: '2000',
+      next: null,
+      operation: null,
+    });
+  });
+
+  test('= is clicked the result should be Division', () => {
+    //Arrange
+    const obj = {
+      total: 100,
+      next: 20,
+      operation: '÷',
+    };
+    const buttonName = '=';
+
+    //Act
+    const result = calculate(obj, buttonName);
+
+    //Assert
+    expect(result).toStrictEqual({
+      total: '5',
+      next: null,
+      operation: null,
+    });
+  });
+
+  test('= is clicked the result should be modulo', () => {
+    //Arrange
+    const obj = {
+      total: 100,
+      next: 20,
+      operation: '%',
+    };
+    const buttonName = '=';
+
+    //Act
+    const result = calculate(obj, buttonName);
+
+    //Assert
+    expect(result).toStrictEqual({
+      total: '0',
+      next: null,
+      operation: null,
+    });
+  });
+
+});
